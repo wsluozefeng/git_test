e09dc258a641878e1a0cf7d3893964876eeb681d
root <root@www.tp.com>
2015-11-23 16:19:53 +0800

    再次测试

--- linux_curl_test.php
+++ linux_curl_test.php
@@ -16,6 +16,7 @@ print_r($_POST);
 echo "<br>\n";
 
 $tmp = file_get_contents( 'php://input' ); //todo POST 请求的情况下，最好使用 php://input 来代替 $HTTP_RAW_POST_DATA（原生的post数据），因为它不依赖于特定的 php.ini配置,内存消耗更少，但是enctype="multipart/form-data" 的时候 php://input 是无效的
+
 print_r($tmp);
 
 echo "<br>\n";
